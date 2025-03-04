c96ef4b634ad39ec3c1603e09b5abd48e5b97cbb
Nguyễn Văn Kiên <138760549+KIEN20030806@users.noreply.github.com>
2025-03-05 00:44:27 +0700

    f

--- rfid.py
+++ rfid.py
@@ -6,13 +6,13 @@ This address needs to match the RFID Module pad:
 [ON :ON ] 0x2F
 '''
 
-import machine
 import json
 from time import sleep, sleep_ms
 from rfid_expansion import *
 import os
 import uos
 from machine import SoftI2C, Pin
+from yolo_uno import *
 
 _SYSNAME = os.uname().sysname
 
