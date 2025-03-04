a625bcebf545cb6418760943f0acf10e628bf8d6
Nguyễn Văn Kiên <138760549+KIEN20030806@users.noreply.github.com>
2025-03-04 23:38:54 +0700

    d

--- definition.js
+++ definition.js
@@ -68,6 +68,29 @@ Blockly.Python['scan_and_add_card'] = function(block) {
   return code;
 };
 
+Blockly.Blocks['scan_and_check'] = {
+  init: function() {
+    this.jsonInit({
+      "type": "scan_and_check",
+      "message0": "quét và kiểm tra thẻ trong danh sách %1%2",
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "LISTNAME",
+          "check": "String"
+        },
+        { "type": "input_dummy" }
+      ],
+      "output": "Boolean",
+      "previousStatement": null,
+      "nextStatement": null,
+      "colour": "#00aeae",
+      "tooltip": "Quét thẻ RFID và kiểm tra có thuộc danh sách không",
+      "helpUrl": ""
+    });
+  }
+};
+
 Blockly.Python['scan_and_check'] = function(block) {
   var listname = Blockly.Python.valueToCode(block, 'LISTNAME', Blockly.Python.ORDER_ATOMIC) || "''";
   Blockly.Python.definitions_['import_rfid'] = 'from rfid import *'; 
@@ -79,18 +102,19 @@ Blockly.Blocks['scan_and_remove_card'] = {
   init: function() {
     this.jsonInit({
       "type": "scan_and_remove_card",
-      "message0": "quét và xóa thẻ khỏi danh sách %1",
+      "message0": "quét và xóa thẻ khỏi danh sách %1%2",
       "args0": [
         {
           "type": "input_value",
           "name": "LISTNAME",
           "check": "String"
-        }
+        },
+        { "type": "input_dummy" }
       ],
       "previousStatement": null,
       "nextStatement": null,
-      "colour": "#ff0000",
-      "tooltip": "Quét thẻ RFID và xóa khỏi danh sách nếu tồn tại",
+      "colour": "#00aeae",
+      "tooltip": "Quét thẻ RFID và xóa thẻ khỏi danh sách",
       "helpUrl": ""
     });
   }
@@ -107,18 +131,19 @@ Blockly.Blocks['clear_list'] = {
   init: function() {
     this.jsonInit({
       "type": "clear_list",
-      "message0": "xóa danh sách %1",
+      "message0": "xóa danh sách %1%2",
       "args0": [
         {
           "type": "input_value",
           "name": "LISTNAME",
           "check": "String"
-        }
+        },
+        { "type": "input_dummy" }
       ],
       "previousStatement": null,
       "nextStatement": null,
-      "colour": "#8B0000",
-      "tooltip": "Xóa danh sách thẻ RFID và file lưu trữ",
+      "colour": "#00aeae",
+      "tooltip": "xóa danh sách",
       "helpUrl": ""
     });
   }
